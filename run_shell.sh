fc942c0fe1aa2039777967f9b27d33201c0eb7df
Felix Semler <68687832+Naderschah@users.noreply.github.com>
2026-02-10 15:32:17 +0100

    Making docker work without X11 available

--- run_shell.sh
+++ run_shell.sh
@@ -1,6 +1,20 @@
 # Get user data 
-export UID=$(id -u) 
+export DOCKER_UID=$(id -u)
 export GID=$(id -g)
 export XEMFEM_PATH=$(pwd)
+
+# ---- X detection & stubbing ----
+if [ -n "${DISPLAY:-}" ] && [ -S /tmp/.X11-unix/X0 ]; then
+    echo "X detected"
+    export ENABLE_X=1
+    export XAUTHORITY=${XAUTHORITY:-$HOME/.Xauthority}
+else
+    echo "X NOT detected, using stubs"
+    export ENABLE_X=0
+    export DISPLAY=
+    export XAUTHORITY=/tmp/.Xauthority.stub
+    touch "$XAUTHORITY"
+fi
+
 docker compose -f ./docker/docker-compose.yml run --rm --build --service-ports mfem-shell
 
